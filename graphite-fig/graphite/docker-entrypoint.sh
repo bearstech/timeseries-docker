d23df28e1e8ddec10edf139d8524b0af80946698
Mathieu Lecarme <mlecarme@bearstech.com>
2015-02-13 15:42:05 +0100

    Entrypoint powered.

--- graphite-fig/graphite/docker-entrypoint.sh
+++ graphite-fig/graphite/docker-entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 cd /opt/graphite/webapp/graphite
 if [ ! -e /opt/graphite/storage/graphite.db ]; then
     /opt/graphite/bin/python manage.py syncdb --noinput
+else
+    echo "Using old database"
 fi
 
 exec "$@"
