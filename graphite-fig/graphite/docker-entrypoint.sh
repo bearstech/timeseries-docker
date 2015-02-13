94d4d0855668ca665c33213e30a9d229122ef56e
Mathieu Lecarme <mlecarme@bearstech.com>
2015-02-13 15:24:52 +0100

    For the very first time.

--- graphite-fig/graphite/docker-entrypoint.sh
+++ graphite-fig/graphite/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+set -e
 
-cd /opt/graphite/webapp/graphite /opt/graphite/bin/python manage.py syncdb
-
+cd /opt/graphite/webapp/graphite
+if [ ! -e /opt/graphite/storage/graphite.db ]; then
+    /opt/graphite/bin/python manage.py syncdb --noinput
+fi
 
 exec "$@"
